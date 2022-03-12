52448439cca97dc943c6363d388e6eab0c318c78
mandie021-moEqdO <mandie021@outlook.com>
2022-03-11 20:06:36 -0500

    final adds

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from sqlalchemy import create_engine, func
 import numpy as np
 
 
-engine = create_engine("postgresql://postgres:postgres@localhost:5432/StockMarket")
+engine = create_engine("postgresql://postgres:Static2$@localhost:5432/StockMarket")
 
 # reflect an existing database into a new model
 Base = automap_base()
--- static/js/app.js
+++ static/js/app.js
@@ -28,11 +28,11 @@ async function get_asset_by_asset_id(asset_id)
     let home_url        = "/get_asset_by_asset_id/" + asset_id;
     const home_response = await fetch(home_url);
     const graph_data    = await home_response.json();
-    chartbuild2040(asset_id, graph_data);
-    chartbuild2050(asset_id, graph_data);
+    buildCharts(asset_id, graph_data);
+    get_description_for_asset_id(asset_id);
 };
 
-function chartbuild2040(asset_id, graph_data)
+function buildCharts(asset_id, graph_data)
 {
     let future_x2050_bucket   = [];
     let y_predict2050_bucket  = [];   
@@ -202,47 +202,26 @@ function chartbuild2040(asset_id, graph_data)
 
 }; //end function
 
-function chartbuild2050(graph_data)
+function get_description_for_asset_id(asset_id)
 {
-     //let home_url = "/get_asset_by_asset_id/" + asset_id;
-     //const home_response = await fetch(home_url);
-     //const data = await home_response.json();
-   
-     var stockticker = data[0];
+    myObject = { 'DIA': "The DIA Exchange Traded Funds (ETF) follows the price and performance of the Dow Jones Industrial Average (DJIA) Index. An exchange-traded fund (ETF) is an investment that's comprised of other investments, like stocks. It's similar to a mutual fund, but ETFs are traded on an exchange.  Notable companies that are apart of the DIA are: Apple, American Express, Home Depot, Coco-Cola, Mcdonalds, Visa and Walmart.", 
+                 'IWM': "IWM is an exchange-traded fund (ETF) that tracks the performance of the Russell 2000 Index. This index tracks select U.S. companies with a smaller market capitalization . The average weighted market cap of the Russell 2000 company was $3.13 billion as of January 2022.  Notable companies that are apart of IWM are: AMC, AVIS Budget, Asana, BJ's Wholesale Club and Texas Roadhouse.", 
+                 'QQQ': "QQQ is an Invesco Exchange Traded Fund (ETF) that tracks the Nasdaq 100.The Nasdaq-100 Index (NDX) is a collection of the largest 100 non-financial companies in the world listed on the Nasdaq exchange.  Notable companies that trade are a part of the QQQ are: Adobe, Amazon, Apple, Microsoft, Pepsi and Tesla. ",
+                 'SPY': "The SPDR S&P 500 is an exchange-traded fund (ETF) that was created to provide an investment vehicle that produces returns roughly in line with the S&P 500 Index before expenses.The S&P 500 index is composed of U.S. companies across all Global Industry Classification Standard (GICS) sectors with an unadjusted market capitalization of $8 billion or greater. Each company in the index must also have had positive earnings in the most recent quarter and over the most recent four quarters.  Notable companies that are apart of the SPY are: Meta Platforms (Facebook), Apple, Google, JP Morgan, Tesla, Disney, Netflix, Nike, Walmart and Chipotle",
+                 'XLB': "XLB DeXLB is an exchange-traded fund (ETF) that tracks the performance of construction materials, metals mining, chemicals and manufacturing companies that trade on the S&P 500.  Notable companies apart of XLB are: Sherwin-Williams, Dow, Linde, Nucor and Freeport-McMoran.scription",
+                 'XLE': "XLE is an exchange-traded fund (ETF) that tracks the performance of the energy companies that trade on the S&P 500.  Notable companies apart of XLE are: Exxon Mobil, Chevron, Philipps 66, Kinder Morgan and Schlumberger.",
+                 'XLF': "XLF is an exchange-traded fund (ETF) that tracks the performance of the financial services companies that trade on the S&P 500.  Notable companies apart of XLF are: Berkshire Hathaway, JP Morgan, Citi, American Express, Morgan Stanley and Goldman Sachs.",
+                 'XLI': "XLI is an exchange-traded fund (ETF) of S&P 500 companies from the following industries: aerospace and defense; industrial conglomerates; marine; transportation infrastructure; machinery; road and rail; air freight and logistics; commercial services and supplies. Notable companies that are apart of XLI are: 3M, General Electric, Union Pacific, Lockheed Martin and Boeing.",
+                 'XLK': "XLK is an exchange-traded fund (ETF) that tracks the performance of technology companies in the S&P 500.  Notable companies apart of XLK are: Apple, Microsoft, Adobe, Intel, PayPal and NVIDIA.",
+                 'XLP': "XLP is an exchange-traded fund (ETF) that tracks the performance of consumer staples companies in the S&P 500. Notable companies apart of XLP are: Coco-Cola, Proctor and Gamble, Pepsi, Costco, Walmart and Altria.",
+                 'XLU': "XLU is an exchange-traded fund (ETF) that tracks the performance of utility companies in the S&P 500. Notable companies apart of XLU are:  NextEra Energy, Duke Energy, Dominion Energy, Exelon and American Water Works.",
+                 'XLV': "XLY is an exchange-traded fund (ETF) that tracks the performance of health care companies in the S&P 500. Notable companies apart of XLV are:  UnitedHealth Group, Johnson & Johnson, Merck, Thermo Fisher, AbbVie and Pfizer.",
+                 'XLY': "XLY is an exchange-traded fund (ETF) that tracks the performance of consumer discretionary companies in the S&P 500. Notable companies apart of XLY are:  Amazon, Mcdonalds, Nike, Target, Booking Holdings, Starbucks, Home Depot and Lowe's."};
+                 
+     description = myObject[asset_id];
 
-     var trace1 =  
-     {
-         x: Xp,
-         y: y,
-         type: 'scatter',
-         label: [stock_id + "History"],
-         color: "blue"
-     };
-    
-     var trace2=  
-     {
-         x: Xp,
-         y: y_learned,
-         type: 'scatter',
-         label: stock_id + "Mathmatical Model",
-         color: "red"
-     };
-    
-     var trace3=  
-     {
-       x: future_x,
-         y: y_predict,
-         type: 'scatter',
-         label: stock_id + "Future Predictions",
-         color: "green"
-     };
-    
-    var chart = [trace1, trace2, trace3];
-
-    var layout = {title:'S&P 500 Materials Sector Price Predictions'};
-    
-    Plotly.newPlot('myDiv', chart, layout);
-}; 
+     document.getElementById('description').innerHTML = description;
+};
 
 async function main()
 {
